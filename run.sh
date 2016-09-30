03d8bbf778f610b03676f86d8bc9d97df1f6e7c7
Jim Vallandingham <vlandham@gmail.com>
2016-09-29 20:49:50 -0700

    add log file

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 DAY=`date +%Y_%m_%d`
 mkdir -p out
+mkdir -p log
 rm -f out/nordstrom_${DAY}.jsonlines
-scrapy crawl nordstrom -o out/nordstrom_${DAY}.jsonlines
+scrapy crawl nordstrom -o out/nordstrom_${DAY}.jsonlines --logfile log/nordstrom_${DAY}.log.txt
